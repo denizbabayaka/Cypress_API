d039eac4123551238876dc7b838c0e223f8f2c70
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-26 16:26:27 -0400

    new files added

--- cypress/integration/network-request.js
+++ cypress/integration/network-request.js
@@ -12,8 +12,8 @@ describe('Network Request', () => {
             //this will look for the any url that has comments at the end of it
             url: '**/comments/*',
         },
-        // here we mock the response body with a json object
-        // this will be the new response body of the intercepted request
+            // here we mock the response body with a json object
+            // this will be the new response body of the intercepted request
             {
 
                 body: {
@@ -34,4 +34,26 @@ describe('Network Request', () => {
         cy.wait('@getComment').its('response.statusCode').should('eq', 200);
     })
 
+    it('Post Request', () => {
+        // we are listening any post request with the url /comments and
+        // store it into an alias called postComment
+
+        cy.intercept("POST", "**/comments**",).as('postComment');
+        cy.get('.network-post').click();
+
+        cy.wait("@postComment").should(({ request, response }) => {
+            console.log(request);
+
+            expect(request.body).to.include('email', 'hello@cypress.io');
+            console.log(response);
+            expect(response.body).to.have.property("name", "Using POST in cy.intercept()");
+            expect(request.headers).to.have.property("content-type");
+            expect(request.headers).to.have.property("origin", "https://example.cypress.io");
+
+
+
+        })
+
+
+    })
 })
